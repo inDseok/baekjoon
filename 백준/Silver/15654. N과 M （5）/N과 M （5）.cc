41a6f0d3feaa6ee263979fcc3ced84fe7dd7ee63
inDseok <inseok99@gmail.com>
2024-05-25 16:14:54 +0900

    [Silver III] Title: N과 M (5), Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int N, M;
+vector<int> v;
+int arr[9];
+int visited[9];
+void print_num(int cnt)
+{
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i]<<" ";
+		}
+		cout << "\n";
+	}
+	else {
+		for (int i = 0; i < N; i++) {
+			if (visited[i] == 0) {
+				visited[i] = 1;
+				arr[cnt] = v[i];
+				print_num(cnt + 1);
+				visited[i] = 0;
+			}
+		}
+	}
+}
+int main()
+{
+	cin >> N >> M;
+	for (int i = 0; i < N; i++){
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	print_num(0);
+}
