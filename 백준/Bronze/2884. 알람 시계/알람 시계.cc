00d62c05253694decfc14dd56bfec3948b694505
inDseok <inseok99@gmail.com>
2024-05-25 16:04:36 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int hour, min;
+	cin >> hour >> min;
+
+	if (min < 45) {
+		hour = hour - 1;
+		if (hour < 0) {
+			hour = 23;
+		}
+		min = min + 60 - 45;
+	}
+	else {
+		min = min - 45;
+	}
+	cout << hour <<" "<< min;
+}
