9e26568d1ea7d6a8e2ab9990d1ed7b7f8eb5d44c
inDseok <inseok99@gmail.com>
2024-05-25 16:06:28 +0900

    [Silver IV] Title: 상근이의 여행, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int t;
+int main()
+{
+	cin >> t;
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+		for (int i = 0; i < m; i++) {
+			int a, b;
+			cin >> a >> b;
+		}
+		cout << n - 1<<"\n";
+	}
+}
